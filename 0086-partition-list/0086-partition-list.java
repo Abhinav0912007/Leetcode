2a4e1e5b57159fae38769fc1262b78167ace7e46
Abhinav Singh <abhinavsingh82005@gmail.com>
2026-07-04 16:07:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (67.9%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode d1 = new ListNode(-1);
+        ListNode d2 = new ListNode(-1);
+
+        ListNode t1 = d1;
+        ListNode t2 = d2;
+
+        ListNode t = head;
+
+        while (t != null) {
+            ListNode next = t.next;
+
+            if (t.val < x) {
+                t1.next = t;
+                t1 = t1.next;
+            } else {
+                t2.next = t;
+                t2 = t2.next;
+            }
+
+            t = next;
+        }
+
+        t2.next = null;
+        t1.next = d2.next;
+
+        return d1.next;
+    }
+}
