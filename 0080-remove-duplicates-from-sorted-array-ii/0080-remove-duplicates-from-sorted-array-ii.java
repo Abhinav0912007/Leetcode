45b1adf8f33c296ccf283d34a0990359c1980dd0
Abhinav Singh <abhinavsingh82005@gmail.com>
2026-07-14 23:26:13 +0530

    Time: 0 ms (100%), Space: 48.8 MB (50.9%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -4,31 +4,29 @@ class Solution {
             return nums.length;
         }
 
-        int i = 1;
-        int j = 0;
-        int len = 1;
-        int count = 1;
-
-        while (i < nums.length) {
-
-            if (nums[i] == nums[i - 1]) {
-                len++;
-
-                if (len <= 2) {
-                    j++;
-                    nums[j] = nums[i];
-                    count++;
-                }
-            } else {
-                len = 1;
+     int i =1;
+     int j =0;
+     int len=1;
+     int count=1;
+     while(i<nums.length){
+        if(nums[i-1]==nums[i]){
+            len++;
+            if(len<=2){
                 j++;
-                nums[j] = nums[i];
+                nums[j]=nums[i];
                 count++;
+             
             }
-
-            i++;
         }
-
-        return count;
+        else{
+            len=1;
+            j++;
+            nums[j]= nums[i];
+           
+            count++;
+        }
+        i++;
+     }
+     return count;
     }
 }
